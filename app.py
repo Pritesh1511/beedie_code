46eea40ac9fac865f8fea397803ec9e762d0bac0
Pritesh1511 <125392659+Pritesh1511@users.noreply.github.com>
2023-02-14 13:56:03 -0800

    dates

--- app.py
+++ app.py
@@ -6,4 +6,13 @@ tickers = ['AAPL','DIS','IBM']
 ticker = st.selectbox("Pick a ticker",tickers)
 df = pd.read_csv(ticker+".csv",parse_dates=['Date'],index_col=['Date'])
 
+begDate = df.index.min()
+endDate = df.index.max()
+
+pickStart =st.date_Input("Pick start date:",begDate)
+pickEnd = st.date_input("Pick start date:",endDate)
+
+
 st.write(df)
+st.write(begDate)
+st.write(endDate)
