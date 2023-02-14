e45ea86cab371fa128b5320e5b702bcf9de96401
Pritesh1511 <125392659+Pritesh1511@users.noreply.github.com>
2023-02-14 14:10:39 -0800

    saca

--- app.py
+++ app.py
@@ -5,6 +5,7 @@ st.write("APP WORKING")
 tickers = ['AAPL','DIS','IBM']
 ticker = st.selectbox("Pick a ticker",tickers)
 df = pd.read_csv(ticker+".csv",parse_dates=['Date'],index_col=['Date'])
+st.write(df)
 
 
 begDate = df.index.min()
@@ -24,6 +25,6 @@ st.plotly_chart(fig)
 
 
 
-st.write(df)
+
 st.write(begDate)
 st.write(endDate)
