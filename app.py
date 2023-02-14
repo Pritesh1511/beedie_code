6c10a3b9c3437849b5272ea2edd24e10e8f799d2
Pritesh1511 <125392659+Pritesh1511@users.noreply.github.com>
2023-02-14 14:14:40 -0800

    changeds

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ st.write("APP WORKING")
 tickers = ['AAPL','DIS','IBM']
 ticker = st.selectbox("Pick a ticker",tickers)
 df = pd.read_csv(ticker+".csv",parse_dates=['Date'],index_col=['Date'])
-st.write(df)
+
 
 
 begDate = df.index.min()
