5894d22ecebf262d27af5d384aea1ea589b86a64
Pritesh1511 <125392659+Pritesh1511@users.noreply.github.com>
2023-02-14 14:09:39 -0800

    fsahk

--- app.py
+++ app.py
@@ -5,20 +5,25 @@ st.write("APP WORKING")
 tickers = ['AAPL','DIS','IBM']
 ticker = st.selectbox("Pick a ticker",tickers)
 df = pd.read_csv(ticker+".csv",parse_dates=['Date'],index_col=['Date'])
+st.write(df)
 
 begDate = df.index.min()
 endDate = df.index.max()
 
 minDateDropdown = pd.to_datetime('2010-01-01')
 
-pickStart =st.date_input("Pick start date:",begDate,min_value = minDateDropdown)
-pickEnd = st.date_input("Pick start date:",endDate)
+pickStart =st.sidebar.date_input("Pick start date:",begDate,min_value = minDateDropdown)
+pickEnd = st.sidebar.date_input("Pick start date:",endDate)
 
 filterDF = df.loc[pickStart:pickEnd]
 st.write(filterDF)
-st.write(df.loc[pickStart:pickEnd])
+
+import plotly.express as px
+fig = px.line(filterDF, x=filterDF.index, y="Close")
+st.plotly_chart(fig)
 
 
-st.write(df)
+
+
 st.write(begDate)
 st.write(endDate)
